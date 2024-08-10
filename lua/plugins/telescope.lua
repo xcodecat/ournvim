3212f5ee2e4e3a8f5ec5bd7c13ac2b916b3d83d3
Leonhard Breuer <me@leobreuer.dev>
2024-08-10 22:04:06 +0200

    make shortcuts better and turn neotree into netrw

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,8 +4,11 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require("telescope.builtin")
-            vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>fs", function()
+                builtin.grep_string({search = vim.fn.input("Grep > ")})
+            end)            
         end
     },
     {
@@ -13,8 +16,8 @@ return {
         config = function()
             require("telescope").setup {
                 extensions = {
-                  ["ui-select"] = {
-                    require("telescope.themes").get_dropdown {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
 
                 }
             }
